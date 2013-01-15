edb19cee65b426d9b33b8dae81e66038728db984
Grant Rodgers <grantr@gmail.com>
2013-01-15 15:13:03 -0800

    don't require that a node id has been set to get the local node
    The special node key :_local always gets the local node. The node id is
    only used so things like routers can dispatch messages to nodes without
    caring whether they are local or not.

--- lib/telecine/node_registry.rb
+++ lib/telecine/node_registry.rb
@@ -5,7 +5,7 @@ module Telecine
   class NodeRegistry < Registry
     def get(key)
       @_lock.synchronize do
-        if key == Telecine.node.id
+        if key == Telecine.node.id || :_local
           # note that this is not published, only available when requested
           fetch(key.to_sym, nil) || store(key.to_sym, LocalNode.new)
         else
@@ -15,7 +15,7 @@ module Telecine
     end
 
     def local
-      get(Telecine.node.id)
+      get(:_local)
     end
   end
 end

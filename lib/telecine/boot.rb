3215953ca4578795b8483399afba01b3f8244342
Grant Rodgers <grantr@gmail.com>
2013-01-17 10:33:55 -0800

    note about services started at boot
    Do we want users to be forced to run them?

--- lib/telecine/boot.rb
+++ lib/telecine/boot.rb
@@ -4,5 +4,6 @@ Celluloid.logger = Celluloid::IncidentLogger.new
 # Celluloid::IncidentReporter.supervise_as :default_incident_reporter, STDERR
 Celluloid::EventReporter.supervise_as :default_event_reporter, STDOUT
 
+#TODO should these be started automatically?
 Telecine::Notifier.supervise_as :remote_notifier
 Telecine::Router.supervise_as :router

8838d4b05283fa0e7055bceec454ae27c94e8220
Grant Rodgers <grantr@gmail.com>
2013-01-16 22:27:16 -0800

    add actor references
    References are handles to remote actors. Avoids the need to deal with
    routers and nodes directly.
    
    For now references are name-based only, meaning the referenced actor
    must be named on the remote node. Eventually mailbox references will be
    supported also.

--- lib/telecine/remote_node.rb
+++ lib/telecine/remote_node.rb
@@ -58,15 +58,33 @@ module Telecine
       "Node id:#{@id} address:#{@address}"
     end
 
-    # call a method and wait for the response
-    def call(destination, method, *args)
-       Celluloid::Actor[:router].call(@id, destination, method, *args)
+    def reference_to(actor, router=:router)
+      # TODO mailbox references
+      Reference.new(id, actor, router)
     end
 
-    # call a method without waiting for the response
-    def cast(destination, method, *args)
-      Celluloid::Actor[:router].cast(@id, destination, method, *args)
-    end
+    class Reference
+      attr_accessor :node_id, :name, :router
 
+      def initialize(node_id, name, router=:router)
+        @node_id = node_id
+        @name = name
+        @router = router
+      end
+
+      # call a method and wait for the response
+      def call(method, *args)
+        router.call(@node_id, @name, method, *args)
+      end
+
+      # cast a method without waiting for the response
+      def cast(method, *args)
+        router.cast(@node_id, @name, method, *args)
+      end
+
+      def router
+        @router.is_a?(Symbol) ? Celluloid::Actor[@router] : @router
+      end
+    end
   end
 end

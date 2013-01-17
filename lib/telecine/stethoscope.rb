dda920420eea0603fd850028edce8ae675162a8a
Grant Rodgers <grantr@gmail.com>
2013-01-16 23:09:58 -0800

    use registry getset to create new nodes
    This avoids a race condition if there is more than one thread creating
    nodes (since getset is atomic).

--- lib/telecine/stethoscope.rb
+++ lib/telecine/stethoscope.rb
@@ -12,11 +12,7 @@ module Telecine
     def record_heartbeat(topic, node_id, node_address, heartbeat)
       Logger.trace "recording heartbeat from #{node_id} #{heartbeat}"
 
-      #TODO possible race condition from other node sources
-      unless node = Telecine.nodes.get(node_id)
-        node = Telecine.nodes.set(node_id, Node.new(node_id, node_address))
-      end
-
+      node = Telecine.nodes.get(node_id) { Node.new(node_id, node_address) }
       node.beat_heart(heartbeat)
     end
   end

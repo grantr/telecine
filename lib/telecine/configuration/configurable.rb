a9b1d29ec6a4b569d7ac1200b7114d94c08d5203
Grant Rodgers <grantr@gmail.com>
2013-01-17 10:46:17 -0800

    remove config_topic from Configurable
    Config just uses ids now

--- lib/telecine/configuration/configurable.rb
+++ lib/telecine/configuration/configurable.rb
@@ -6,33 +6,15 @@ module Telecine
     end
 
     module ClassMethods
-      def config_topic(topic=nil)
-        if topic
-          @config_topic = topic
-        else
-          @config_topic
-        end
-      end
-
       def config
         # create a new "anonymous" class that will host the compiled reader methods
-        @config ||= Class.new(Configuration).new(@_config_topic)
+        @config ||= Class.new(Configuration).new
       end
 
       def configure
         yield config
       end
 
-      #TODO there should also be a way to slave a local config accessor to some other config, eg
-      # class SomeClass
-      #   use_config :master_server, from: Telecine.config
-      #
-      #   def foo
-      #     puts master_server
-      #   end
-      # end
-      # variables defined this way would probably just be thunks to the config object. no local state.
-      # really, just like config_accessor but with a remote receiver.
       def config_accessor(*names)
         options = names.last.is_a?(Hash) ? names.pop : {}
 

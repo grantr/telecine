57d55ef49ec57ddcc0d21eecf25ec9a0ea144fe1
Grant Rodgers <grantr@gmail.com>
2013-01-22 09:46:19 -0800

    notes about making boot more flexible
    Users may not be using zmq, and may be using different implementations
    of base actors.
    
    Ultimately boot.rb should probably go away and be replaced by some kind
    of dependency mechanism.

--- lib/telecine/boot.rb
+++ lib/telecine/boot.rb
@@ -1,12 +1,15 @@
+#TODO start this when necessary. Some apps might not use zmq at all.
 Celluloid::ZMQ.init
 
+  #TODO allow the user to choose what logging tech they want
 Celluloid.logger = Celluloid::IncidentLogger.new
 # Celluloid::IncidentReporter.supervise_as :default_incident_reporter, STDERR
 Celluloid::EventReporter.supervise_as :default_event_reporter, STDOUT
 
 #TODO these should not be started automatically as some apps might not need them
-# or might use different implementations. In fact everything in this file should
-# be optional.
+# or might use different implementations.
+# TODO Would be nice to have a dependency mechanism. Some way for actors to declare
+# that they depend on certain functionality that other actors provide.
 Telecine::Notifier.supervise_as :remote_notifier
 Telecine::Router.supervise_as :router
 Telecine::Dispatcher.supervise_as :dispatcher

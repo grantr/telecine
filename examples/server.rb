ff7b14393f04c5e734944668114f20061ef1d1a3
Grant Rodgers <grantr@gmail.com>
2013-01-25 20:38:40 -0800

    add explicit registration to Dispatcher
    Instead of allowing any actor to be referenceable, only look for the
    ones that have explicitly registered themselves.

--- examples/server.rb
+++ examples/server.rb
@@ -5,5 +5,6 @@ require './examples/server-config'
 require './examples/test_actor'
 
 ta = TestActor.supervise_as :test_actor
+Celluloid::Actor[:dispatcher].register(ta.actors.first, :test_actor)
 
 Celluloid::Actor.join(Celluloid::Actor[:remote_notifier])
--- lib/telecine/dispatcher.rb
+++ lib/telecine/dispatcher.rb
@@ -2,27 +2,41 @@ module Telecine
   class Dispatcher
     include Celluloid
 
+    def registry
+      @registry ||= Registry.new
+    end
+
     # this class takes care of finding actors to dispatch method calls to, and returning the result if necessary.
     # it manages actors publishing capabilities and external capabilities incoming from the router. It hands out capabilities to remote nodes (ie bootstrap)
     #
     # actors publish which methods they want accessible, and any parameters
 
-    def call(destination, method, *args)
-      if mailbox = find_mailbox(destination)
+    def register(actor, reference_id)
+      Logger.debug("registering #{actor.mailbox.inspect} as #{reference_id}")
+      registry.set(reference_id, actor.mailbox)
+    end
+
+    def call(reference_id, method, *args)
+      Logger.debug("got call: #{reference_id}, #{method}, #{args.inspect}")
+      if mailbox = find_mailbox(reference_id)
         Celluloid::Actor.call(mailbox, method, *args)
       end
     end
 
-    def cast(destination, method, *args)
-      if mailbox = find_mailbox(destination)
+    def cast(reference_id, method, *args)
+      if mailbox = find_mailbox(reference_id)
         Celluloid::Actor.async(mailbox, method, *args)
       end
     end
 
-    def find_mailbox(destination)
-      actor = Celluloid::Actor[destination.to_sym]
-      actor && actor.alive? ? actor.mailbox : nil
-      #TODO return a mailbox if asked
+    #TODO this should dereference supervisors
+    def find_mailbox(reference_id)
+      Logger.debug "looking for #{reference_id.inspect}"
+      Logger.debug "registry is #{registry.inspect}"
+      reference = registry.get(reference_id)
+      Logger.debug("got #{reference.inspect} as #{reference_id}")
+
+      reference if reference && reference.alive?
     end
   end
 end

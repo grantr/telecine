3230524e4236bbe00d0cb99af0ace79bf06f5915
Grant Rodgers <grantr@gmail.com>
2013-02-21 12:51:34 -0800

    move time serializer to its own file

---
+++ lib/telecine/serializer/time.rb
@@ -0,0 +1,11 @@
+class Time
+  include Telecine::Serializable
+
+  def as_primitive(options={})
+    xmlschema(6)
+  end
+
+  def self.from_primitive(representation, options={})
+    parse(representation)
+  end
+end

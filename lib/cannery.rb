6e8200f96986c5464f6a13fffbdb67b84691ec20
Grant Rodgers <grantr@gmail.com>
2013-02-25 19:41:44 -0800

    add some docs for Cannery
    Also change Cannery#dump to return Can#dump instead of the can object.
    More likely to be what users want, and there's no reason to expose the
    Can object if we don't have to.

--- lib/cannery.rb
+++ lib/cannery.rb
@@ -1,6 +1,13 @@
 class Cannery
-  # This class is the container for serializers and state.
-  attr_accessor :serializers, :id_to_class, :class_to_id
+  # This class is the container for serializers. It stores the mapping between
+  # classes and serializers, and classes and ids.
+  
+  # Maps classes to serializers.
+  attr_accessor :serializers
+  
+  # Map class ids to classes and back again. Class ids are the class name by
+  # default.
+  attr_accessor :id_to_class, :class_to_id
 
   def initialize
     @serializers = {}
@@ -8,24 +15,49 @@ class Cannery
     @class_to_id = {}
   end
 
+  # Register a serializer for a class with an optional class id. The serializer
+  # should be an instance of a serializer, not a serializer class. The class id
+  # defaults to the class name if not present.
+  #
+  # Examples:
+  #
+  # register(String, IdentitySerializer.new)
+  # register(MyObject, MyObjectSerializer.new, 2)
+  #
+  # Returns the assigned class id.
   def register(klass, serializer, id=nil)
     @serializers[klass] = serializer
     @id_to_class[id || klass.name] = klass
     @class_to_id[klass] = id || klass.name
   end
 
+  # Can an object.
+  # To can an object is to flatten the object graph and translate each object
+  # into primitives that can be used to deserialize the object on the other end.
+  # @see Can
+  #
+  # Returns the canned object suitable for serializing with any serialization library.
+  # Raises UnserializableObject if an object is canned without a matching
+  # serializer for its class.
   def can(object)
     can = Can.new(self)
     can.add(object)
-    can
+    can.dump
   end
 
+  # Uncan an object. The object must have been previously canned by a Cannery.
+  #
+  # Returns the uncanned object.
+  # Raises UnserializableObject if an object is uncanned without a matching
+  # serializer for its class.
   def uncan(primitive)
     can = Can.new(self, primitive)
     can.load
   end
 
-  # could maybe have a default serializer
+  # Retrieve the serializer for a class or class id.
+  #
+  # Returns the serializer or nil if no serializer was found.
   def serializer_for(class_or_id)
     if class_or_id.is_a?(Class)
       @serializers[class_or_id]
@@ -34,16 +66,22 @@ class Cannery
     end
   end
 
+  # Retrieve the class id for a given class.
+  #
+  # Returns the class id or nil if no class id was found.
   def id_for(klass)
     @class_to_id[klass]
   end
 
+  # Dup the serializer and class id hashes on copy so that the copied object
+  # will get its own hashes.
   def initialize_copy(other)
     @serializers = @serializers.dup
     @id_to_class = @id_to_class.dup
     @class_to_id = @class_to_id.dup
   end
 
+  # Raised when an object in a Can has no corresponding serializer.
   class UnserializableObject < StandardError; end
 
   class Can
@@ -55,7 +93,11 @@ class Cannery
       @counter = 0
     end
 
-    #TODO this should (optionally?) raise if something can't be serialized
+    # Add an object to the can. The object is assigned an id, serialized,
+    # and added to the can hash.
+    #
+    # Returns the object id.
+    # Raises UnserializableObject if no serializer can be found for an object.
     def add(object)
       id = (@counter += 1)
       class_id = @cannery.id_for(object.class)
@@ -67,18 +109,32 @@ class Cannery
       end
     end
 
+    # dump the Can.
+    # Returns a hash suitable for serializing with any serialization library.
     def dump
       @primitive
     end
 
+    # Load the object from the primitive in the Can.
+    #
+    # Returns the uncanned object.
+    # Raises UnserializableObject if no serializer can be found for an object.
     def load
       @objects = {}
       @primitive.inject(nil) do |accum, (object_id, thing)|
         class_id, object = thing.first
-        @objects[object_id.to_s] = @cannery.serializer_for(class_id).load(self, object) #TODO should to_s be needed here?
+        if serializer = @cannery.serializer_for(class_id)
+          @objects[object_id.to_s] = serializer.load(self, object) #TODO should to_s be needed here?
+        else
+          raise UnserializableObject, "#{object.class} cannot be serialized. Did you register a serializer for it?"
+        end
       end
     end
 
+    # Find an object in the can by id.
+    # This will be called by serializers looking for objects by reference.
+    #
+    # Returns the object or nil if not found.
     def find(id)
       @objects[id.to_s] #TODO should to_s be needed here?
     end

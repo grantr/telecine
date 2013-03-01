56ced20ec5a1506dbb230a4aa8f0a181fa8ac60d
Grant Rodgers <grantr@gmail.com>
2013-03-01 11:34:00 -0800

    don't depend on Address class in BasicResolver

--- lib/telecine/resolver.rb
+++ lib/telecine/resolver.rb
@@ -1,23 +1,26 @@
 module Telecine
   module Resolver
     # The resolver should:
-    #   - take a message
-    #   - return a mailbox if the message is valid and can be delivered
-    #   - return an error if the message is invalid
-    #   - return an error or dead-letter mailbox if the message is valid
+    #   - take a envelope
+    #   - return a mailbox if the envelope is valid and can be delivered
+    #   - return an error if the envelope is invalid
+    #   - return an error or dead-letter mailbox if the envelope is valid
     #     but cannot be delivered
     #   - add middleware to the mailbox returned (by wrapping the real mailbox
     #     with a proxy mailbox)
+    #   - cache mailboxes by address
   end
 
   class BasicResolver
     include Resolver
 
-    def resolve(message)
-      address = message.destination.mailbox_id
+    def resolve(envelope)
+      mailbox_address = envelope.destination.split("@").first
 
-      actor = Celluloid::Actor.all.detect { |a| a.mailbox.address == address }
-      actor.mailbox
+      puts "looking for mailbox #{mailbox_address}"
+
+      actor = Celluloid::Actor.all.detect { |a| a.mailbox.address == mailbox_address }
+      actor.mailbox if actor
     end
   end
 end
